b21e30cb6c67afbbf23a9a89fa987675638eb1e7
farabi-deriv <farabi@regentmarkets.com>
2025-12-02 13:34:22 +0800

    fix: title header flickering

--- src/templates/_common/components/title.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { getBrandName } from '../brand.config';
-
-const Title = () => (
-    <title>{`SmartTrader | Online trading platform | ${getBrandName()}`}</title>
-);
-
-export default Title;
