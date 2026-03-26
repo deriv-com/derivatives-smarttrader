a1c8de66b9e053289ebf84e5e40f6a1032f0c530
farabi-deriv <farabi@regentmarkets.com>
2026-03-27 04:08:10 +0800

    fix: mobile header

--- src/javascript/app/components/header.jsx
+++ src/javascript/app/components/header.jsx
@@ -224,6 +224,7 @@ export const init = () => {
             <AppProvider>
                 <>
                     <SidebarComponent />
+                    <HeaderComponent />
                     <LanguageMenuModal />
                     <MobileMenuComponent />
                     <BottomNavComponent />
